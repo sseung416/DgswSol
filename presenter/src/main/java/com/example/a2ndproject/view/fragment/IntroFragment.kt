30149d22df178ed13a793ec9be06badfae007636
tmddusqls <tmddusqls@naver.com>
2021-08-28 13:19:30 +0900

    [Update] Splash

---
+++ presenter/src/main/java/com/example/a2ndproject/view/fragment/IntroFragment.kt
@@ -0,0 +1,33 @@
+package com.example.a2ndproject.view.fragment
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.a2ndproject.R
+import com.example.a2ndproject.view.viewmodel.IntroViewModel
+
+class IntroFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = IntroFragment()
+    }
+
+    private lateinit var viewModel: IntroViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.intro_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(IntroViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
