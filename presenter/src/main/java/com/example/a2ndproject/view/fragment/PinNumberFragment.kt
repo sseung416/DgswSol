0655da6cdc54d56d651853353e9b28e183ffe07f
tmddusqls <tmddusqls@naver.com>
2021-08-31 20:16:06 +0900

    [Update] PinNumberFragment

--- presenter/src/main/java/com/example/a2ndproject/view/fragment/PinNumberFragment.kt
+++ presenter/src/main/java/com/example/a2ndproject/view/fragment/PinNumberFragment.kt
@@ -1,20 +1,27 @@
 package com.example.a2ndproject.view.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.example.a2ndproject.R
 import com.example.a2ndproject.databinding.PinNumberFragmentBinding
 import com.example.a2ndproject.view.viewmodel.PinNumberViewModel
+import java.util.*
 
-class PinNumberFragment : Fragment() {
+class PinNumberFragment : Fragment(), View.OnClickListener {
 
     private lateinit var binding: PinNumberFragmentBinding
     private lateinit var viewModel: PinNumberViewModel
+
+    private var numberButtons: MutableList<Button> = mutableListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,46 +37,61 @@ class PinNumberFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setButton(view)
 
+        numberButtons.forEach {
+            it.setOnClickListener(this)
+        }
+        binding.btnDeletePinNumber.setOnClickListener(this)
     }
 
     private fun init() {
         viewModel = ViewModelProvider(requireActivity()).get(PinNumberViewModel::class.java)
     }
 
-    private fun setBtn() {
-        binding.btn0PinNumber.setOnClickListener {
+    private fun setButton(v: View) {
+        for (i in 0..9) {
+            val id = "btn_${i}_pin_number"
 
+            numberButtons.add(v.findViewById(resources.getIdentifier(id, "id", requireActivity().packageName)))
         }
-
-        binding.btn1PinNumber.setOnClickListener {
-
-        }
-
-        binding.btn2PinNumber.setOnClickListener {
-
-        }
-
-        binding.btn3PinNumber.setOnClickListener {
-
-        }
-
-        binding.btn4PinNumber.setOnClickListener {
-
-        }
-
-        binding.btn5PinNumber.setOnClickListener {
-
-        }
-
-        binding.btn6PinNumber.setOnClickListener {
-
-        }
-
 
     }
 
-    private fun fillPin() {
+    private var pinNumber: Stack<Int> = Stack()
 
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            binding.btnDeletePinNumber.id -> {
+                if (pinNumber.size != 0)  {
+                    fillPin(true)
+
+                    Log.d("pinNumber", "pop: ${pinNumber.pop()}")
+                }
+            }
+
+            // deleteButton이 아닌 숫자 패드 클릭시 실행
+            // resource id로 resource name 구하고, name에서 숫자만 추출해 pinNumber에 저장
+            else -> {
+                val number = v.resources.getResourceEntryName(v.id).replace(("[^0-9]").toRegex(), "").toInt()
+
+                Log.d("pinNumber", "click: $number")
+                pinNumber.push(number)
+
+                fillPin(false)
+            }
+        }
     }
+    
+    private fun fillPin(isDelete: Boolean) {
+        val id = "cv_pin${pinNumber.size}_pin_number"
+        val cardView: CardView = requireView().findViewById(resources.getIdentifier(id, "id", requireActivity().packageName))
+        val color: Int = when (isDelete) {
+            true -> R.color.grey
+            false -> R.color.black
+        }
+
+        cardView.setCardBackgroundColor(ContextCompat.getColor(requireContext(), color))
+    }
+
 }
