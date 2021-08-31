f15396b5bef2bfd8e69bf4f9258dddb19c180ebb
tmddusqls <tmddusqls@naver.com>
2021-08-31 21:05:35 +0900

    [Update] AddAccountFragment

---
+++ presenter/src/main/java/com/example/a2ndproject/view/fragment/AddAccount1Fragment.kt
@@ -0,0 +1,51 @@
+package com.example.a2ndproject.view.fragment
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.example.a2ndproject.R
+import com.example.a2ndproject.databinding.AddAccount1FragmentBinding
+
+class AddAccount1Fragment : Fragment() {
+
+    private val navController: NavController by lazy {
+        findNavController()
+    }
+
+    private lateinit var binding: AddAccount1FragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.add_account1_fragment, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnRegisterAddAccount1.setOnClickListener {
+            navigateToNewThisAccount()
+        }
+
+        binding.btnConnectAddAccount1.setOnClickListener {
+            navigateToConnectThisAccount()
+        }
+    }
+
+    private fun navigateToConnectThisAccount() {
+        navController.navigate(R.id.action_addAccount1Fragment_to_connectThisAccountFragment)
+    }
+
+    private fun navigateToNewThisAccount() {
+        navController.navigate(R.id.action_addAccount1Fragment_to_newThisAccountFragment)
+    }
+}
---
+++ presenter/src/main/java/com/example/a2ndproject/view/fragment/NewThisAccountFragment.kt
@@ -0,0 +1,24 @@
+package com.example.a2ndproject.view.fragment
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.a2ndproject.R
+
+class NewThisAccountFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.new_this_account_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
