7240a463032447b8cbbe8ce401e39e0cf4541264
tmddusqls <tmddusqls@naver.com>
2021-09-01 10:12:13 +0900

    [Update] signUpFragment xml 변경 및 PinNumberFragment로 연결 추가

---
+++ presenter/src/main/java/com/example/a2ndproject/view/fragment/SignUpFragment.kt
@@ -0,0 +1,54 @@
+package com.example.a2ndproject.view.fragment
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.example.a2ndproject.R
+import com.example.a2ndproject.databinding.SignUpFragmentBinding
+import com.example.a2ndproject.view.viewmodel.SignUpViewModel
+
+class SignUpFragment : Fragment() {
+
+    private val navController by lazy {
+        findNavController()
+    }
+
+    private lateinit var binding: SignUpFragmentBinding
+    private lateinit var viewModel: SignUpViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(layoutInflater, R.layout.sign_up_fragment, container, false)
+
+        init()
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnConfirmSignUp.setOnClickListener {
+            navigateToPinNumber()
+        }
+
+    }
+
+    private fun init() {
+        viewModel = ViewModelProvider(requireActivity()).get(SignUpViewModel::class.java)
+    }
+
+    private fun navigateToPinNumber() {
+        val bundle = Bundle()
+        bundle.putInt("type", 0)
+
+        navController.navigate(R.id.action_signUpFragment_to_pinNuberFragment, bundle)
+    }
+}
---
+++ presenter/src/main/java/com/example/a2ndproject/view/viewmodel/SignUpViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.a2ndproject.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class SignUpViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
