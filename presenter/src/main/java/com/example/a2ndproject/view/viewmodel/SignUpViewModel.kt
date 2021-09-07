51d725d876734b1b0c7be93e99dc9011df3c52e8
tmddusqls <tmddusqls@naver.com>
2021-09-07 19:32:37 +0900

    [Update] SignUpViewModel.kt

--- presenter/src/main/java/com/example/a2ndproject/view/viewmodel/SignUpViewModel.kt
+++ presenter/src/main/java/com/example/a2ndproject/view/viewmodel/SignUpViewModel.kt
@@ -2,7 +2,7 @@
  * Sign Up ViewModel Class
  *
  * @author 최승연
- * @date 2021-09-06
+ * @date 2021-09-07
  * */
 package com.example.a2ndproject.view.viewmodel
 
@@ -11,13 +11,56 @@ import androidx.lifecycle.ViewModel
 
 class SignUpViewModel : ViewModel() {
 
-    private lateinit var _inputError: MutableLiveData<String>
+    val isDoubleCheckedId = MutableLiveData<Boolean>()
+    val isDoubleCheckedPassword = MutableLiveData<Boolean>()
 
-    fun isValidId(id: String?): Boolean {
-        return true
+    /**
+     * 아이디 정규식 검사 메서드
+     *
+     * @return String? : 에러 메세지를 반환(에러가 없을 시 null)
+     * @param id : 아이디
+     * */
+    fun isValidId(id: String): String? {
+        /* 영어, 숫자를 포함한 3~12글자의 정규식 */
+        val reg = "^(?=.*[A-Za-z])(?=.*[0-9])[A-Za-z[0-9]]{3,12}".toRegex()
+
+        if (id.isBlank()) return "아이디를 입력해주세요."
+        else if (!id.matches(reg)) return "영문자 및 숫자를 포함한 3~12글자를 입력해주세요."
+
+        return null
     }
 
-    fun isValidPassword(password: String?): Boolean {
-        return true
+    /**
+     * 비밀번호 정규식 검사 메서드
+     *
+     * @return String? : 에러 메세지를 반환(에러가 없을 시 null)
+     * @param password : 비밀번호
+     * */
+    fun isValidPassword(password: String): String? {
+        /* 영어, 숫자, 특수기후를 모두 포함한 8~12글자의 정규식 */
+        val reg = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{8,12}$".toRegex()
+
+        if (password.isBlank()) return "비밀번호를 입력해주세요."
+        else if (!password.matches(reg)) return "영문자, 숫자, 특수기호를 모두 포함한 8~12글자를 입력해주세요."
+
+        return null
     }
+
+    /**
+     * 닉네임 정규식 검사 메서드
+     *
+     * @return String? : 에러 메세지를 반환(에러가 없을 시 null)
+     * @param nickName : 닉네임
+     * */
+    fun isValidNickName(nickName: String): String? {
+        /* 영어, 한글, 숫자의 2글자 이상의 조합의 정규식 */
+        // TODO 닉네임 한도 정하기
+        val reg = "^[a-zA-Z가-힣[0-9]]{2,100}".toRegex()
+
+        if (nickName.isBlank()) return "닉네임을 입력해주세요."
+        else if (!nickName.matches(reg)) return "2글자 이상의 닉네임을 입력해주세요."
+
+        return null
+    }
+
 }
