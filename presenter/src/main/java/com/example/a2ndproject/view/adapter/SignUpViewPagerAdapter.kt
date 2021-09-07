c588c3d5a6a651d4ddaf7be7b013adb0d5aa1ab9
tmddusqls <tmddusqls@naver.com>
2021-09-07 13:34:16 +0900

    [Update] SignUpFragment ViewPager swipe enabled

--- presenter/src/main/java/com/example/a2ndproject/view/adapter/SignUpViewPagerAdapter.kt
+++ presenter/src/main/java/com/example/a2ndproject/view/adapter/SignUpViewPagerAdapter.kt
@@ -1,25 +1,40 @@
 /**
  * 회원가입의 ViewPager를 연결할 Adapter Class
+ * View Binding 사용
  *
  * @author 최승연
- * @date 2021-09-06
+ * @date 2021-09-07
  * */
 package com.example.a2ndproject.view.adapter
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentContainerView
 import androidx.recyclerview.widget.RecyclerView
-import androidx.viewpager2.widget.ViewPager2
-import com.example.a2ndproject.R
 import com.example.a2ndproject.databinding.ViewPagerItemSignUpFragmentBinding
 
 class SignUpViewPagerAdapter : RecyclerView.Adapter<SignUpViewPagerAdapter.ViewHolder>() {
     private lateinit var binding: ViewPagerItemSignUpFragmentBinding
 
-    inner class ViewHolder : RecyclerView.ViewHolder(binding.root)
+    inner class ViewHolder : RecyclerView.ViewHolder(binding.root) {
+
+        /**
+         * view의 visibility 변경
+         *
+         * @param position : ViewPager의 position
+         *  */
+        fun setLayout(position: Int) {
+            binding.constraintLayout1ViewPagerItemSignUp.visibility = View.GONE
+            binding.constraintLayout2ViewPagerItemSignUp.visibility = View.GONE
+            binding.constraintLayout3ViewPagerItemSignUp.visibility = View.GONE
+
+            when (position) {
+                0 -> binding.constraintLayout1ViewPagerItemSignUp.visibility = View.VISIBLE
+                1 -> binding.constraintLayout2ViewPagerItemSignUp.visibility = View.VISIBLE
+                2 -> binding.constraintLayout3ViewPagerItemSignUp.visibility = View.VISIBLE
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         binding = ViewPagerItemSignUpFragmentBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -27,25 +42,10 @@ class SignUpViewPagerAdapter : RecyclerView.Adapter<SignUpViewPagerAdapter.ViewH
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        setLayout(position)
+        holder.setLayout(position)
     }
 
     override fun getItemCount(): Int = 3
 
-    /**
-     * ViewPager의 position이 바뀌었을 때 호출되어 view의 visibility를 변경
-     *
-     * @param position : ViewPager가 바뀌었을 때의 position
-     *  */
-    private fun setLayout(position: Int) {
-        binding.constraintLayout1ViewPagerItemSignUp.visibility = View.GONE
-        binding.constraintLayout2ViewPagerItemSignUp.visibility = View.GONE
-        binding.constraintLayout3ViewPagerItemSignUp.visibility = View.GONE
 
-        when (position) {
-            0 -> binding.constraintLayout1ViewPagerItemSignUp.visibility = View.VISIBLE
-            1 -> binding.constraintLayout2ViewPagerItemSignUp.visibility = View.VISIBLE
-            2 -> binding.constraintLayout3ViewPagerItemSignUp.visibility = View.VISIBLE
-        }
-    }
 }
--- presenter/src/main/java/com/example/a2ndproject/view/fragment/SignUpFragment.kt
+++ presenter/src/main/java/com/example/a2ndproject/view/fragment/SignUpFragment.kt
@@ -3,7 +3,7 @@
  * 아이디, 비밀번호, 휴대폰 번호, 주민등록번호(7번째까지), 이름(실명), 별명, 프로필 사진 필요
  *
  * @author 최승연
- * @date 2021-09-06
+ * @date 2021-09-07
  * */
 package com.example.a2ndproject.view.fragment
 
@@ -17,6 +17,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.SCROLL_STATE_DRAGGING
 import com.example.a2ndproject.R
 import com.example.a2ndproject.databinding.SignUpFragmentBinding
 import com.example.a2ndproject.view.adapter.SignUpViewPagerAdapter
@@ -38,9 +39,7 @@ class SignUpFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(layoutInflater, R.layout.sign_up_fragment, container, false)
-
         init()
-
         return binding.root
     }
 
@@ -51,7 +50,16 @@ class SignUpFragment : Fragment() {
 
         /* 확인 버튼 클릭 시 ViewPager의 다음 페이지로 넘어감 */
         binding.btnConfirmSignUp.setOnClickListener {
-            binding.viewPagerSignUp.currentItem = binding.viewPagerSignUp.currentItem + 1
+            when (binding.viewPagerSignUp.currentItem) {
+                /* 마지막 페이지에서 버튼의 text를 가입하기로 바꾸기 위함 */
+                1 -> binding.btnConfirmSignUp.text = "가입하기"
+
+                /* 마지막 페이지에서 회원가입 완료 후 핀번호 설정 페이지로 넘어감 */
+                2 -> navigateToPinNumber()
+
+                /* 다음 페이지로 넘기기 위해 currentItem을 현재 currentItem에 1을 더함 */
+                else -> binding.viewPagerSignUp.currentItem = binding.viewPagerSignUp.currentItem + 1
+            }
         }
     }
 
@@ -59,11 +67,20 @@ class SignUpFragment : Fragment() {
         viewModel = ViewModelProvider(requireActivity()).get(SignUpViewModel::class.java)
     }
 
+    /**
+     * ViewPager를 초기화하는 메서드 - adapter, isUserInputEnabled 설정
+     * */
     private fun initViewPager() {
         adapter = SignUpViewPagerAdapter()
         binding.viewPagerSignUp.adapter = adapter
+
+        /* ViewPager의 스와이프를 막음 */
+        binding.viewPagerSignUp.isUserInputEnabled = false
     }
 
+    /**
+     * 회원가입 후 핀번호 설정하도록 PinNumberFragment로 이동하는 메서드
+     * */
     private fun navigateToPinNumber() {
         /* 핀 번호를 설정 하는 지, 로그인 하는 지에 따라 type이 달라짐 */
         val bundle = Bundle()
