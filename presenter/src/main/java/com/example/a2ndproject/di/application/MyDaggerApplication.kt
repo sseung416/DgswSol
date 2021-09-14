b55c8ae2e93f5088a3e88aa900c98d9de060df74
tmddusqls <tmddusqls@naver.com>
2021-09-14 21:09:37 +0900

    feat: di setting

---
+++ presenter/src/main/java/com/example/a2ndproject/di/application/MyDaggerApplication.kt
@@ -0,0 +1,8 @@
+package com.example.a2ndproject.di.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyDaggerApplication : Application() {
+}
---
+++ presenter/src/main/java/com/example/a2ndproject/di/module/NetworkModule.kt
@@ -0,0 +1,21 @@
+package com.example.a2ndproject.di.module
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit =
+        Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("")
+            .build()
+}
