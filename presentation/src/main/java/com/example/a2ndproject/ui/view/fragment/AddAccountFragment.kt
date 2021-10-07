2cc8ab1db2c4a890f900eb259600f89ea728da66
tmddusqls <tmddusqls@naver.com>
2021-10-07 17:19:20 +0900

    update: 계좌 개설 - AddAccountFragment.kt

--- presentation/src/main/java/com/example/a2ndproject/ui/view/fragment/AddAccountFragment.kt
+++ presentation/src/main/java/com/example/a2ndproject/ui/view/fragment/AddAccountFragment.kt
@@ -1,50 +1,41 @@
 package com.example.a2ndproject.ui.view.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.NavController
-import androidx.navigation.fragment.findNavController
 import com.example.a2ndproject.R
-import com.example.a2ndproject.databinding.AddAccount1FragmentBinding
+import com.example.a2ndproject.databinding.AddAccountFragmentBinding
+import com.example.a2ndproject.ui.view.base.BaseFragment
+import com.example.a2ndproject.ui.viewmodel.fragment.AddAccountViewModel
 
-class AddAccountFragment : Fragment() {
+class AddAccountFragment : BaseFragment<AddAccountFragmentBinding, AddAccountViewModel>() {
 
-    private val navController: NavController by lazy {
-        findNavController()
-    }
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): AddAccountFragmentBinding =
+        AddAccountFragmentBinding.inflate(inflater, container, false)
 
-    private lateinit var binding: AddAccount1FragmentBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.add_account_fragment, container, false)
-
-        return binding.root
-    }
+    override fun getViewModelClass(): Class<AddAccountViewModel> = AddAccountViewModel::class.java
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnRegisterAddAccount1.setOnClickListener {
-            navigateToNewThisAccount()
+        binding.btnRegisterAddAccount.setOnClickListener {
+            navigateToCreateAccount()
         }
 
-        binding.btnConnectAddAccount1.setOnClickListener {
+        binding.btnConnectAddAccount.setOnClickListener {
             navigateToConnectThisAccount()
         }
     }
 
     private fun navigateToConnectThisAccount() {
-        navController.navigate(R.id.action_addAccount1Fragment_to_connectThisAccountFragment)
+        navController.navigate(R.id.action_addAccountFragment_to_connectThisAccountFragment)
     }
 
-    private fun navigateToNewThisAccount() {
-        navController.navigate(R.id.action_addAccount1Fragment_to_newThisAccountFragment)
+    private fun navigateToCreateAccount() {
+        navController.navigate(R.id.action_addAccountFragment_to_createAccountFragment)
     }
 }
---
+++ presentation/src/main/java/com/example/a2ndproject/ui/viewmodel/fragment/AddAccountViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.a2ndproject.ui.viewmodel.fragment
+
+import com.example.a2ndproject.ui.view.base.BaseViewModel
+
+class AddAccountViewModel : BaseViewModel() {
+
+}
