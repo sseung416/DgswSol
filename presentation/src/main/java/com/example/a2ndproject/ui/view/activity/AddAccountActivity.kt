cbab66013832b402125392b7020dc56fa408fdfc
tmddusqls <tmddusqls@naver.com>
2021-10-07 16:17:42 +0900

    update: 계좌 개설 액티비티 구현 (+ 툴바)

--- presentation/src/main/java/com/example/a2ndproject/ui/view/activity/AddAccountActivity.kt
+++ presentation/src/main/java/com/example/a2ndproject/ui/view/activity/AddAccountActivity.kt
@@ -1,12 +1,33 @@
 package com.example.a2ndproject.ui.view.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import com.example.a2ndproject.R
+import com.example.a2ndproject.databinding.ActivityAddAccountBinding
 
-class AddAccountActivity : AppCompatActivity() {
+class AddAccountActivity : AppCompatActivity(), Toolbar.OnMenuItemClickListener {
+
+    private lateinit var binding: ActivityAddAccountBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityAddAccountBinding.inflate(layoutInflater)
+        setSupportActionBar(binding.toolbarAddAccount)
         setContentView(R.layout.activity_add_account)
+
+        binding.toolbarAddAccount.setOnMenuItemClickListener(this)
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.toolbar_btn_close_add_account -> {
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+        }
+
+        return true
     }
 }
