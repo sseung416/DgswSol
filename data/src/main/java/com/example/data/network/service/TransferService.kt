418724e8739c6718c0b558cabe6904367aa709dc
tmddusqls <tmddusqls@naver.com>
2021-10-09 14:06:15 +0900

    update: data - transferService

--- data/src/main/java/com/example/data/network/service/TransferService.kt
+++ data/src/main/java/com/example/data/network/service/TransferService.kt
@@ -1,4 +1,24 @@
 package com.example.data.network.service
 
+import com.example.domain.entity.request.Transfer
+import com.example.domain.entity.request.TransferPw
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
 interface TransferService {
+
+    // 송금 계좌 확인
+    @GET("/send/check/{receive}")
+    suspend fun getTransferCheck(): Response<String>
+
+    // 송금
+    @POST("/send")
+    suspend fun postTransfer(@Body transfer: Transfer): Response<String>
+
+    // 송금 비밀번호 확인
+    @POST("/send/pw")
+    suspend fun postTransferPw(@Body transferPw: TransferPw): Response<String>
 }
