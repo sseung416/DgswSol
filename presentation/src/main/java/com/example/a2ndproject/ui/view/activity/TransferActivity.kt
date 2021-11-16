0eec56e10952026c0c0343014e51260001da1e8d
tmddusqls <tmddusqls@naver.com>
2021-11-16 20:35:49 +0900

    update: TransferActivity init

---
+++ presentation/src/main/java/com/example/a2ndproject/ui/view/activity/TransferActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a2ndproject.ui.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a2ndproject.R
+
+class TransferActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transfer)
+    }
+}
