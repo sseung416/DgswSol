08c0e38e82270f59ca2edb41714be6c9589f66a8
tmddusqls <tmddusqls@naver.com>
2021-11-21 20:19:19 +0900

    update: CheckInfoFragment 프로필 가져오기 적용

--- presentation/src/main/java/com/example/a2ndproject/ui/view/fragment/CheckInfoFragment.kt
+++ presentation/src/main/java/com/example/a2ndproject/ui/view/fragment/CheckInfoFragment.kt
@@ -2,30 +2,52 @@ package com.example.a2ndproject.ui.view.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.example.a2ndproject.R
 import com.example.a2ndproject.databinding.CreateAccountCheckInfoFragmentBinding
 import com.example.a2ndproject.ui.view.base.BaseFragment
 import com.example.a2ndproject.ui.view.data.FragmentType
 import com.example.a2ndproject.ui.view.utils.MessageUtil
+import com.example.a2ndproject.ui.viewmodel.fragment.CheckInfoViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CheckInfoFragment : BaseFragment<CreateAccountCheckInfoFragmentBinding>() {
+
+    private val viewModel: CheckInfoViewModel by viewModels()
 
     override fun getLayoutResId(): Int =
         R.layout.create_account_check_info_fragment
 
-    override fun setViewModel() {}
+    override fun setViewModel() {
+        binding.vm = viewModel
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        observe()
+
+        viewModel.getProfile()
+
         binding.btnConfirmCheckInfo.setOnClickListener {
             if (binding.etCheckInfo.text!!.isNotBlank()) {
                 navController.navigate(CheckInfoFragmentDirections.actionCheckInfoFragmentToNumberPadPinFragment(
-                    FragmentType.PIN_ACCOUNT_PW.type, binding.etCheckInfo.text.toString()
+                    FragmentType.PIN_ACCOUNT_PW.type, viewModel.accountName.value!!
                 ))
             } else {
                 MessageUtil.showDialog(requireActivity(), "별명을 입력해주세요.")
             }
+        }
+    }
+
+    private fun observe() = with(viewModel) {
+        isSuccess.observe(viewLifecycleOwner) {
+            profile.value = it
+        }
+
+        isFailure.observe(viewLifecycleOwner) {
+            MessageUtil.showDialog(requireActivity(), "서버 통신에 실패하였습니다.")
         }
     }
 }
--- presentation/src/main/java/com/example/a2ndproject/ui/viewmodel/fragment/CheckInfoViewModel.kt
+++ presentation/src/main/java/com/example/a2ndproject/ui/viewmodel/fragment/CheckInfoViewModel.kt
@@ -1,12 +1,37 @@
 package com.example.a2ndproject.ui.viewmodel.fragment
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.domain.entity.response.Profile
+import com.example.domain.usecase.user.GetProfileUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
+import java.lang.Exception
+import javax.inject.Inject
 
-class CheckInfoViewModel : ViewModel() {
+@HiltViewModel
+class CheckInfoViewModel @Inject constructor(
+    private val getProfileUseCase: GetProfileUseCase
+) : ViewModel() {
     val accountName = MutableLiveData<String>()
 
-    val navigateToPin = MutableLiveData(false)
+    val isSuccess = MutableLiveData<Profile>()
+    val isFailure = MutableLiveData<String>()
+
+    val profile = MutableLiveData<Profile>()
+
+    fun getProfile() {
+        try {
+            viewModelScope.launch {
+                isSuccess.postValue(getProfileUseCase.buildUseCase())
+            }
+        } catch (e: Exception) {
+            Log.d("getProfile", e.message.toString())
+            isFailure.postValue(e.message.toString())
+        }
+
+    }
 }
