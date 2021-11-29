f8c0f1841a296d07cefe5ecd634ec5bdc6da5bb4
tmddusqls <tmddusqls@naver.com>
2021-11-29 10:11:14 +0900

    update: 송금 ui 구현

--- presentation/src/main/java/com/example/a2ndproject/ui/view/fragment/TransferCheckFragment.kt
+++ presentation/src/main/java/com/example/a2ndproject/ui/view/fragment/TransferCheckFragment.kt
@@ -6,28 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.a2ndproject.R
+import com.example.a2ndproject.databinding.TransferCheckFragmentBinding
+import com.example.a2ndproject.ui.view.base.BaseFragment
 import com.example.a2ndproject.ui.viewmodel.fragment.TransferCheckViewModel
 
-class TransferCheckFragment : Fragment() {
+class TransferCheckFragment : BaseFragment<TransferCheckFragmentBinding>() {
 
-    companion object {
-        fun newInstance() = TransferCheckFragment()
-    }
+    override fun getLayoutResId(): Int =
+        R.layout.transfer_check_fragment
 
-    private lateinit var viewModel: TransferCheckViewModel
+    override fun setViewModel() {}
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.transfer_check_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(TransferCheckViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
+    private val viewModel: TransferCheckViewModel by viewModels()
 }
